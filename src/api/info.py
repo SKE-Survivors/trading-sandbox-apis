3dcee48a7e703111dd2bc9e05b4ccda65c8fd4c9
Nanthakarn Limkool <nanthakarn18@gmail.com>
2022-12-18 23:27:17 +0700

    fix(info): add pairs

--- src/api/info.py
+++ src/api/info.py
@@ -19,8 +19,19 @@ def index():
 @info_endpoint.route("/pairs", methods=["GET"])
 @cross_origin()
 def pairs():
-    # todo: something (?)
-    data = ["btc-usdt", "eth-usdt", "bnb-usdt"]
+    data = [
+        "bnb-busd", "btc-busd", "aud-busd", "ada-busd", "xrp-busd", "ltc-busd",
+        "eth-busd", "sol-busd", "cake-busd", "gmt-busd", "shib-busd",
+        "doge-busd", "bnb-usdt", "btc-usdt", "aud-usdt", "ada-usdt",
+        "xrp-usdt", "ltc-usdt", "eth-usdt", "sol-usdt", "cake-usdt",
+        "gmt-usdt", "shib-usdt", "doge-usdt", "ada-bnb", "xrp-bnb", "ltc-bnb",
+        "sol-bnb", "cake-bnb", "gmt-bnb", "bnb-btc", "ada-btc", "xrp-btc",
+        "ltc-btc", "eth-btc", "sol-btc", "cake-btc", "gmt-btc", "doge-btc",
+        "bnb-dai", "btc-dai", "eth-dai", "busd-rub", "usdt-rub", "bnb-rub",
+        "btc-rub", "ada-rub", "xrp-rub", "ltc-rub", "eth-rub", "sol-rub",
+        "doge-rub", "bnb-aud", "btc-aud", "ada-aud", "xrp-aud", "eth-aud",
+        "sol-aud", "gmt-aud", "shib-aud", "doge-aud", "bnb-eth", "shib-doge"
+    ]
     body = {"STATUS": "SUCCESS", "MESSAGE": data}
     return build_response(status_code=200, body=body)
 
