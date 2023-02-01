4b8f39b777afa3cbeacf7606576a3c09b3c0b0d3
Nanthakarn Limkool <nanthakarn18@gmail.com>
2023-02-01 09:01:47 +0700

    fix(service): apply new DatabaseHandler

--- src/api/service.py
+++ src/api/service.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, request
 from flask_cors import CORS, cross_origin
 from api.constants.message import *
-from model import Order, Trigger
+from handler import DatabaseHandler
 from utils import build_response
 
 service_endpoint = Blueprint('service', __name__)
 CORS(service_endpoint)
+
+dbh = DatabaseHandler()
 
 
 @service_endpoint.route('/')
@@ -37,8 +39,8 @@ def update_market():
             return build_response(status_code=400, body=FAILED_REQUIRE_PRICE)
 
         try:
-            orders = Order.redis_get_orders_at(pair_symbol, float(price))
-            map(lambda o: o.execute_order(), orders)
+            orders = dbh.redis_get_orders_at(pair_symbol, float(price))
+            map(lambda o: dbh.execute(o), orders)
         except Exception as err:
             body = {
                 "STATUS": "FAILED",
@@ -47,8 +49,8 @@ def update_market():
             return build_response(status_code=400, body=body)
 
         try:
-            triggers = Trigger.redis_get_triggers_at(pair_symbol, float(price))
-            map(lambda t: t.trigger(), triggers)
+            triggers = dbh.redis_get_triggers_at(pair_symbol, float(price))
+            map(lambda t: dbh.trigger(t), triggers)
         except Exception as err:
             body = {
                 "STATUS": "FAILED",
